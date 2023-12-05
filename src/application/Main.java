d7e722ab61acf804f7a49f8b357685e456c84f08
Felipe <felipe.baleche2@gmail.com>
2023-12-05 10:38:15 -0300

    added tests in main program

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,35 @@
 package application;
 
+import dao.Dao;
+import dao.DaoFactory;
+import dao.implementation.SellerDao;
+import entities.Department;
+import entities.Seller;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
+
+        Dao<Seller> sellerDao = DaoFactory.createSellerDao();
+        Dao<Department> departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("#### Test seller find all ####");
+        List<Seller> sellerList1 = sellerDao.findAll();
+        sellerList1.forEach(System.out::println);
+        System.out.println();
+
+
+        System.out.println("#### Test seller find by department ####");
+        List<Seller> sellerList2 = ((SellerDao)sellerDao).findByDepartment(new Department(2, "Electronics"));
+        sellerList2.forEach(System.out::println);
+        System.out.println();
+
+
+        System.out.println("#### Test seller find by id ####");
+        Seller seller = sellerDao.findById(2);
+        System.out.println(seller);
+        System.out.println();
 
 
 
