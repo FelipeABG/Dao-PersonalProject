fbd9bfbbbfb61b94ca4c6bdebe00768ea74a1efc
Felipe <felipe.baleche2@gmail.com>
2023-12-05 09:57:11 -0300

    added readme

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,5 @@
 package application;
 
-import dao.Dao;
-import dao.DaoFactory;
-import dao.implementation.SellerDao;
-import entities.Department;
-import entities.Seller;
-import java.util.List;
-
-
 public class Main {
     public static void main(String[] args){
 
